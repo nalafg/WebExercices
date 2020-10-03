d17e788e749517d9006a8b0c5cc25e9add034abd
Alan Piña <nalafurfagenti@gmail.com>
2020-10-02 22:06:41 -0600

    commit wakamole

--- bostrap.php
+++ bostrap.php
@@ -1,3 +1,26 @@
+<?php 
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "wakamole_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT ID, nombre, apellido, correo, estado FROM usuarios";
+$result = $conn->query($sql);
+ ?>
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -202,6 +225,124 @@
     </div>
   </div>
 </div>
+
+
+
+<! –– formulario ––>
+
+
+
+	<div class="super_div">
+		
+		<center>
+			<form id="formulario">
+				<fieldset style="width: 40%;">
+					<legend>
+						Datos del usuario
+					</legend>
+					<label> email: </label>
+					<input type="text" style="width: 100%;height: 40px;" name="email" id="email" value="" placeholder="">
+
+					<label>contraseña</label>
+
+					<input type="text" style="width: 100%;height: 40px;" name="password" id="password" value="" placeholder="">
+
+					<label>año de nacimiento</label>
+
+					<input type="text" style="width: 100%;height: 40px;" name="year" id="year" value="" placeholder="">
+
+					<button type="button" id="guardar_info"> Guardar regisro</button>
+				</fieldset>
+			</form>
+
+
+
+			<form id="formulario">
+				<section style="width: 80%; margin-top: 20px">
+					<legend>
+						Registro de Usuarios
+					</legend>
+
+					<table class="egt" style="border: 1px;width: 100%;" border="5px">
+						<tr>
+							<td>ID</td>
+						    <td>Email</td>
+						    <th>Pasword</th>
+						    <th>Year</th>
+						</tr>
+
+
+
+						<?php 
+						if ($result->num_rows > 0) {
+						  // output data of each row
+						  while($row = $result->fetch_assoc()) {
+						 ?>
+
+						<tr>
+							<td><?php echo $row['ID'] ?></td>
+							<td><?php echo $row['nombre'] ?></td>
+							<td><?php echo $row['apellido'] ?></td>
+							<td><?php echo $row['correo'] ?></td>
+							<td><?php echo $row['estado'] ?></td>
+						</tr>
+
+						<?php 
+						  }
+						} else {
+						  echo "0 results";
+						}
+						$conn->close();
+						?>
+
+
+					</table>
+
+				</section>
+			</form>
+		
+			
+		</center>
+		<h1 id="edad_h1">  </h1>
+	</div>
+
+
+
+
+			
+  
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script type="text/javascript">
+	
+	  
+	$("#guardar_info").click(function(){
+	  	console.log("presionaste el boton de guardar");
+	  	var email = $("#email").val();
+	  	var pass = $("#password").val();
+	  	var year = $("#year").val();
+	  	var num = 0;
+	  	$.ajax({
+	  		url:'file.php',
+	  		data: {email,pass,year},
+	  		type:'POST',
+	  		dataType:'text',
+	  		success:function(r){
+	  			//console.log(r);
+	  			//$("read_h1").text(r);
+
+
+	  		}
+	  	})
+		    
+	  	location.reload();
+  	});
+	 
+
+
+
+
+
 
 </body>
 </html>
